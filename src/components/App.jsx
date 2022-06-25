f39b90ac458043161398761606f3fbf1f981c929
埃米呵呵呵 <hokoribeikaka@hokoribeikakanoMacBook-Air.local>
2022-06-25 17:21:43 +0900

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+
+
+  return (
+    <div className="df flex-col vh-100">
+    <title>profile</title>
+      <h1>Profile</h1>
+      <p>of XIAO YUNYI testtesttesttest</p>
+
+    </div>
+  )
+}
